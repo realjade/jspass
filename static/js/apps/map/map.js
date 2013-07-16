1683eb0f6a7dffb25aa6a08caefa7ba55686c473
zhangyy-g <zhangyy-g@grandsoft.com.cn>
2013-07-16 12:31:15 +0800

    commit

--- static/js/apps/map/map.js
+++ static/js/apps/map/map.js
@@ -1,15 +1,29 @@
 $(function(){
 	var map = new BMap.Map("map");    
 	var point = new BMap.Point(116.404, 39.915);
+	tools.log(point);
 	map.centerAndZoom(point, 15);  // 编写自定义函数，创建标注
-	/*function myFun(result){
+	function myFun(result){
 		tools.log(result);
 	    var cityName = result.name;
+	    $('.city').html(cityName);
 	    map.setCenter(cityName);
+	    var point = result.center;
+	    var lat = point.lat,
+			lng = point.lng;
+		var polyline = new BMap.Polyline([
+		  new BMap.Point(lng, lat),
+		  new BMap.Point(lng+0.006,lat+0.010),
+		  new BMap.Point(lng+0.0365,lat-0.010)
+		], {strokeColor:"blue", strokeWeight:6, strokeOpacity:0.5});
+		map.addOverlay(polyline);
+		var listener1 = BMapLib.EventWrapper.addListener(polyline, 'mouseover', function(e){
+		    alert('鼠标经过啦');
+		});
 	}
 	var myCity = new BMap.LocalCity();
 	myCity.get(myFun);
-	var listener1 = BMapLib.EventWrapper.addListener(map, 'click', function(e){
+	/*var listener1 = BMapLib.EventWrapper.addListener(map, 'click', function(e){
 	    showEvent(e);
 	});
 	var eventList = $('#event-list');
@@ -23,13 +37,5 @@ $(function(){
 	    eventList.html(newLine + '<br />' + eventList.html());
 	}
 	map.centerAndZoom(point, 15);*/
-	var polyline = new BMap.Polyline([
-	  new BMap.Point(116.399, 39.910),
-	  new BMap.Point(116.405, 39.920),
-	  new BMap.Point(116.425, 39.900)
-	], {strokeColor:"blue", strokeWeight:6, strokeOpacity:0.5});
-	map.addOverlay(polyline);
-	var listener1 = BMapLib.EventWrapper.addListener(polyline, 'mouseover', function(e){
-	    tools.log(e);
-	});
+	
 });
